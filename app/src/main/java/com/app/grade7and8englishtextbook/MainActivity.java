f9a114dbd6f481e274d08e2acc4c5fd4291d4164
biruk <biruksolomonkd08@gmail.com>
2022-10-03 15:07:57 -0400

    ads

--- app/src/main/java/com/app/grade7and8englishtextbook/MainActivity.java
+++ app/src/main/java/com/app/grade7and8englishtextbook/MainActivity.java
@@ -9,16 +9,38 @@ import android.view.MenuItem;
 
 import android.widget.ImageView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView button=findViewById(R.id.imageButton);
         ImageView button1=findViewById(R.id.imageButton2);
+        AdView adView = new AdView(this);
+
+        adView.setAdSize(AdSize.BANNER);
+
+        adView.setAdUnitId("ca-app-pub-7070396777212212/8062074537");
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         button.setOnClickListener(view -> {
             Intent intent= new Intent(getApplicationContext(),MainActivity2.class);
--- app/src/main/java/com/app/grade7and8englishtextbook/MainActivity2.java
+++ app/src/main/java/com/app/grade7and8englishtextbook/MainActivity2.java
@@ -1,8 +1,9 @@
 package com.app.grade7and8englishtextbook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
@@ -10,6 +11,7 @@ import com.github.barteksc.pdfviewer.PDFView;
 public class MainActivity2 extends AppCompatActivity {
 
     PDFView pdfView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +22,23 @@ public class MainActivity2 extends AppCompatActivity {
         int book_position=getIntent().getIntExtra("key_position",0);
 
         if (book_position==0){
+            ActionBar actionBar = getSupportActionBar();
+            assert actionBar != null;
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setTitle("Grade 7 English Textbook");
 
             pdfView.fromAsset("English G 7 .pdf").fitEachPage(true).load();
 
 
         }
         else if (book_position==1){
+            ActionBar actionBar = getSupportActionBar();
+            assert actionBar != null;
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setTitle("Grade 8 English Textbook");
+
             pdfView.fromAsset("English G 8 .pdf").fitEachPage(true).load();
         }
     }
