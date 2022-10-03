c5c2dccc930699ee68eb239bae7eb51eb3ec22ae
biruk <biruksolomonkd08@gmail.com>
2022-10-03 03:43:15 -0400

    action_bar

--- app/src/main/java/com/app/grade7and8englishtextbook/MainActivity.java
+++ app/src/main/java/com/app/grade7and8englishtextbook/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import android.widget.ImageView;
@@ -31,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_share) {
             Intent sharingIntent = new Intent(Intent.ACTION_SEND);
@@ -56,4 +63,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
